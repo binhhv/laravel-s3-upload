8e7085061386ae3441c988db30e5482df66b54fb
binhhv <binhhv@blackwind.vn>
2023-02-08 23:06:47 +0700

    add

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,8 +46,8 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'key' => null,
+            'secret' => null,
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
