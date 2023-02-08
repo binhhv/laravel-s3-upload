63e7e6103f2345f75f99dd2fab7b65d8d33df543
binhhv <binhhv@blackwind.vn>
2023-02-08 22:46:35 +0700

    add

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
 
 class ImageUploadController extends Controller
 {
@@ -36,7 +37,7 @@ class ImageUploadController extends Controller
                 $file = $request->file('image');
                 $name = time() . $file->getClientOriginalName();
                 $filePath = 'images/' . $name;
-
+                Log::error($filePath);
                 Storage::disk('s3')->put($filePath, file_get_contents($file));
 
                 return back()->with('success', 'Image aaaa Uploaded successfully');
